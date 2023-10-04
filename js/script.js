0956477e3fc38824a890a6f8005d321cf1307d5b
TiJun <9tomek7@gmail.com>
2023-10-04 21:23:23 +0200

    created logic for bad letter click

--- js/script.js
+++ js/script.js
@@ -2,40 +2,62 @@ const loseInfo = document.querySelector('.lose-info')
 const gameButton = document.querySelector('.game-button')
 const gameState = document.querySelector('.game-state')
 const letterButtons = document.querySelectorAll('.keyboard__button')
-const gallowsBottom = document.querySelector('.hangman__gallows--bottom')
-const gallowsMiddle = document.querySelector('.hangman__gallows--middle')
-const gallowsTop = document.querySelector('.hangman__gallows--top')
-const gallowsRope = document.querySelector('.hangman__gallows--rope')
-const head = document.querySelector('.hangman__head')
-const body = document.querySelector('.hangman__body')
-const leftArm = document.querySelector('.hangman__arm--left')
-const rightArm = document.querySelector('.hangman__arm--right')
-const leftLeg = document.querySelector('.hangman__leg--left')
-const rightLeg = document.querySelector('.hangman__leg--right')
+// const gallowsBottom = document.querySelector('.hangman__gallows--bottom')
+// const gallowsMiddle = document.querySelector('.hangman__gallows--middle')
+// const gallowsTop = document.querySelector('.hangman__gallows--top')
+// const gallowsRope = document.querySelector('.hangman__gallows--rope')
+// const head = document.querySelector('.hangman__head')
+// const body = document.querySelector('.hangman__body')
+// const leftArm = document.querySelector('.hangman__arm--left')
+// const rightArm = document.querySelector('.hangman__arm--right')
+// const leftLeg = document.querySelector('.hangman__leg--left')
+// const rightLeg = document.querySelector('.hangman__leg--right')
+const gallow = document.querySelectorAll('#bad-answers-state')
 const category = document.querySelector('.word-to-guess__category')
-const wordToGuess = document.querySelector('.word-to-guess__word')
+const wordToGuessElement = document.querySelector('.word-to-guess__word')
 const words = {
 	countries: ['Poland', 'Spain', 'Estonia'],
 	animals: ['bird', 'cat', 'dog', 'fish'],
 }
-let randomWord
+let wordToGuess
 let randomCategory
 const getRandomWord = () => {
 	randomCategory = Object.keys(words)[Math.floor(Math.random() * Object.keys(words).length)]
-	randomWord = words[randomCategory][Math.floor(Math.random() * words[randomCategory].length)]
-	category.textContent = randomCategory.toUpperCase()
-	return randomWord
+	wordToGuess = words[randomCategory][Math.floor(Math.random() * words[randomCategory].length)].split()[0].toUpperCase()
+	category.append(randomCategory.toUpperCase())
+	console.log(wordToGuess)
+	return wordToGuess
 }
-const hideWordToGuess = () => {
-	for (let letters of randomWord) {
-		let hiddenLetters = '_'
-        wordToGuess.append(hiddenLetters)
+// const hideWordToGuess = () => {
+// 	const hiddenLetter = []
+// 	let underline = '_'
+// 	for (let letters of wordToGuess) {
+// 		// hiddenLetter.push(underline)
+// 		wordToGuessElement.append(hiddenLetter)
+// 	}
+// }
+let badAnswer = -1
+const checkCompatibility = e => {
+	const clickedLetter = e.target.textContent
+	if (wordToGuess.includes(clickedLetter)) {
+		wordToGuessElement.append(clickedLetter)
+	} else {
+		badAnswer++
+		gallow[badAnswer].classList.remove('in-active')
+		console.log(badAnswer)
 	}
 }
 const activeGame = () => {
 	gameState.classList.remove('in-active')
 	gameButton.classList.add('in-active')
 	getRandomWord()
-	hideWordToGuess()
+	// hideWordToGuess()
 }
 gameButton.addEventListener('click', activeGame)
+letterButtons.forEach(button => {
+	button.addEventListener('click', e => {
+		checkCompatibility(e)
+		button.classList.add('disabled-button')
+		button.setAttribute('disabled', 'true')
+	})
+})
